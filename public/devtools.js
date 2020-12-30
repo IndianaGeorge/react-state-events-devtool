75eb40294e4f8614014ac49de8681309261479ce
Jorge Fuente-Alba <AsPerSpecUnity@gmail.com>
2020-12-30 00:22:41 -0300

    Basic extension

---
+++ public/devtools.js
@@ -0,0 +1,9 @@
+chrome.devtools.panels.create(
+    "React State Events",
+    "tempicon.png",
+    "index.html",
+    function(panel) {
+        console.log("This is panel speaking");
+    // code invoked on panel creation
+    }
+);
